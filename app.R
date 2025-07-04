d2e0184110843ca75b455d0ab621a88d1cf1eb66
Guillaume Mulier <DRY12@hotmail.fr>
2025-07-04 16:15:07 +0200

    Introduction of the app

--- app.R
+++ app.R
@@ -19,8 +19,19 @@ ui <- page_navbar(
   nav_panel("Get started", 
             fluidPage(
               p("This application aims at computing probabilities given a known distribution (for example posterior probabilities in bayesian analysis.",
-                "It is separated in 2 parts:"
-                )
+                "It is separated in 2 parts:"),
+              tags$ul(
+                tags$li("One distribution"),
+                tags$li("Difference between 2 distributions")
+              ),
+              br(),
+              p("For both parts, the usage is the same.",
+                "You choose your distribution, then you select values for the parameters (for the difference between 2 distributions, the parameters are for X1-X2).",
+                "You can adjust the graphical window for the plot and the desired probabilities that you want (Pr(X>s) for example.",
+                "Click on Compute Results to display the plot and the asked probabilities.",
+                "You can retrieve these results using the code displayed below the plot and table."),
+              br(), br(),
+              tags$i("Autor: G. Mulier")
             )
             ), 
   nav_panel("One distribution", UniqueDist("DistUnique")), 
